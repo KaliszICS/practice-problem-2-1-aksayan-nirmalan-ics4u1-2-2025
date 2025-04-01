88891ee4cfaf83d0c14ca92eed83f5d77074ba9d
aksayan nirmalan-ics4u1-2 <aksayan.nirmalan@student.tdsb.on.ca>
2025-04-01 14:00:01 +0000

    save changes

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,51 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static int find(int[] array, int number) {
 
+		//int [] numArray={1,7,5,3,8,5};
+
+		//int num=5;
+		int index=-1;
+
+		int occurances=0;
+		for (int i =0; i < array.length; i++){
+			if(array[i]==number){
+				occurances++;
+				index = i;
+				i=array.length;
+			}
+		}
+		return index;
+
+	}
+
+	public static int findLast(String[] array, String string){
+		int index=-1;
+
+		int occurances=0;
+		for(int i=0;i<array.length;i++){
+			if(array[i]==string){
+				occurances++;
+				index=i;
+				
+
+			}
+		}
+		return index;
+	}
+
+	public static int findSecond(char[] array, char character){
+		int index=-1;
+
+		int occurances=0;
+		for(int i=0;i<array.length;i++){
+			if(array[i]==character && occurances < 2){
+				occurances++;
+				index=i;
+				
+			}
+		}
+		return index;
 	}
 
 	
